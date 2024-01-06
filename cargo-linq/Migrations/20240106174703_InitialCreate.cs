67be1cc6b90f354b61b8356e85119ab9b8aeb7cc
golden-state0fmind <areyes2009@yahoo.com>
2024-01-06 11:10:48 -0700

    creates a new migration for db

---
+++ cargo-linq/Migrations/20240106174703_InitialCreate.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace cargo_linq.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Truck",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    LicensePlate = table.Column<string>(type: "TEXT", nullable: false),
+                    DriverName = table.Column<string>(type: "TEXT", nullable: false),
+                    TruckModel = table.Column<string>(type: "TEXT", nullable: false),
+                    TruckMileage = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Truck", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Truck");
+        }
+    }
+}
