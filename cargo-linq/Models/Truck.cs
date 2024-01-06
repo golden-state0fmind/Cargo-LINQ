f3e62a934f9a2a06cf4fd60014cc401228d51a6c
golden-state0fmind <areyes2009@yahoo.com>
2024-01-06 11:07:34 -0700

    adds new fields to freight truck model

--- cargo-linq/Models/Truck.cs
+++ cargo-linq/Models/Truck.cs
@@ -6,7 +6,9 @@ namespace cargolinq.Models
     public class Truck
     {
         public int Id { get; set; }
-        public string LicensePlate { get; set; }
-        public string DriverName { get; set; }
+        public string LicensePlate { get; set; } = string.Empty;
+        public string DriverName { get; set; } = string.Empty;
+        public string TruckModel { get; set; } = string.Empty;
+        public string TruckMileage { get; set; } = string.Empty;
     }
 }
