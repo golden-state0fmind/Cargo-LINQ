751c776d5411c9dce44a8d9eb411bdd599686045
golden-state0fmind <areyes2009@yahoo.com>
2024-01-11 12:26:39 -0700

    updates db context after new models migrated to sqlite, sharing db context considering small project, otherwise would create new db context

--- cargo-linq/Data/TruckDbContext.cs
+++ cargo-linq/Data/TruckDbContext.cs
@@ -9,10 +9,12 @@ namespace cargolinq.Data
 {
     public class TruckDbContext : DbContext
     {
-        public TruckDbContext(DbContextOptions<TruckDbContext> options)
-        : base(options)
+        public TruckDbContext(DbContextOptions<TruckDbContext> options) : base(options)
         {
         }
         public DbSet<Truck> Truck { get; set; } = default!;
+        public DbSet<TruckParts> TruckParts { get; set; } = default!;
+        public DbSet<Supplier> Suppliers { get; set; } = default!;
+        public DbSet<TruckParts> SuppliedParts { get; set; } = default!;
     }
 }
