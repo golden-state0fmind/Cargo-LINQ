130adb78ec8600ea0680b06c3741792869ea14ef
golden-state0fmind <areyes2009@yahoo.com>
2024-01-11 12:25:13 -0700

    new snapshot after migrating new schema to sqlite

--- cargo-linq/Migrations/TruckDbContextModelSnapshot.cs
+++ cargo-linq/Migrations/TruckDbContextModelSnapshot.cs
@@ -16,6 +16,29 @@ namespace cargo_linq.Migrations
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "8.0.0");
 
+            modelBuilder.Entity("cargolinq.Models.Supplier", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("SupplierEmail")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("SupplierName")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("SupplierPhone")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.HasKey("Id");
+
+                    b.ToTable("Suppliers");
+                });
+
             modelBuilder.Entity("cargolinq.Models.Truck", b =>
                 {
                     b.Property<int>("Id")
@@ -41,6 +64,52 @@ namespace cargo_linq.Migrations
                     b.HasKey("Id");
 
                     b.ToTable("Truck");
+                });
+
+            modelBuilder.Entity("cargolinq.Models.TruckParts", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("INTEGER");
+
+                    b.Property<string>("PartName")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<string>("PartNumber")
+                        .IsRequired()
+                        .HasColumnType("TEXT");
+
+                    b.Property<double>("PartPrice")
+                        .HasColumnType("REAL");
+
+                    b.Property<int>("PartQuantity")
+                        .HasColumnType("INTEGER");
+
+                    b.Property<int>("SupplierId")
+                        .HasColumnType("INTEGER");
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("SupplierId");
+
+                    b.ToTable("TruckParts");
+                });
+
+            modelBuilder.Entity("cargolinq.Models.TruckParts", b =>
+                {
+                    b.HasOne("cargolinq.Models.Supplier", "Supplier")
+                        .WithMany("SuppliedParts")
+                        .HasForeignKey("SupplierId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
+
+                    b.Navigation("Supplier");
+                });
+
+            modelBuilder.Entity("cargolinq.Models.Supplier", b =>
+                {
+                    b.Navigation("SuppliedParts");
                 });
 #pragma warning restore 612, 618
         }
