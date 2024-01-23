f1ba2a91f05ff97707a4b12833aefe9869a2f5d1
golden-state0fmind <areyes2009@yahoo.com>
2024-01-23 12:20:22 -0700

    revised the model associations

--- cargo-linq/Models/TruckParts.cs
+++ cargo-linq/Models/TruckParts.cs
@@ -5,20 +5,25 @@ namespace cargolinq.Models
 {
     public class Supplier
     {
-        public int Id { get; set; }
+        [Key]
+        public int SupplierId { get; set; }
+
         public string SupplierName { get; set; } = string.Empty;
         public string SupplierEmail { get; set; } = string.Empty;
         public string SupplierPhone { get; set; } = string.Empty;
-        public ICollection<TruckParts> SuppliedParts { get; set; } = new List<TruckParts>();
+        public virtual List<TruckParts> SuppliedParts { get; set; } = new List<TruckParts>();
     }
     public class TruckParts
     {
-        public int Id { get; set; }
+        [Key]
+        public int TruckPartId { get; set; }
+
         public string PartNumber { get; set; } = string.Empty;
         public string PartName { get; set; } = string.Empty;
         public int PartQuantity { get; set; } = 0;
         public double PartPrice { get; set; } = 0.00;
+
         public int SupplierId { get; set; }
-        public Supplier? Supplier { get; set; }
+        public virtual Supplier? Supplier { get; set; }
     }
 }
