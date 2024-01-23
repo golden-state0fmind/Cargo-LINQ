0eb8bc7ae0a9f29cc2bf231cfc0250f57207bef3
golden-state0fmind <areyes2009@yahoo.com>
2024-01-23 12:18:42 -0700

    starts adding the create model needed for the create page

---
+++ cargo-linq/Pages/Parts/Create.cshtml.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using cargolinq.Data;
+using cargolinq.Models;
+
+namespace cargolinq.Pages.Parts
+{
+    public class CreateModel : PageModel
+    {
+        // Decalring a field only accessible to this class
+        private readonly cargolinq.Data.TruckDbContext _context;
+
+        // Adding method to this class that accepts a param and assigns _context a value
+        public CreateModel(cargolinq.Data.TruckDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public TruckParts Part { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.TruckParts.Add(Part);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./index");
+        }
+
+    }
+}
