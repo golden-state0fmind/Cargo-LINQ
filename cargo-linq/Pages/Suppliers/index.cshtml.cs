f09f3012939fa4fd422267b2985d86301ec1f963
golden-state0fmind <areyes2009@yahoo.com>
2024-01-24 23:54:49 -0700

    starts the suppliers folder, index model and index page

---
+++ cargo-linq/Pages/Suppliers/index.cshtml.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using cargolinq.Data;
+using cargolinq.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace cargolinq.Pages.Parts
+{
+
+}
